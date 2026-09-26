f4a3ed4479ed2e4ab791cf64a03e225820e3fc04
confessess <svvxx521@gmail.com>
2026-09-26 00:00:34 -0400

    Add MM2 and FTAP URLs to hubgui.lua

--- hubgui.lua
+++ hubgui.lua
@@ -1,5 +1,7 @@
 local ArsenalURL = "https://raw.githubusercontent.com/confessess/AR097125409721047210947/main/main.lua"
 local BloxStrikeURL = "https://raw.githubusercontent.com/confessess/BS097510971057105710957/main/main.lua"
+local MM2URL = "https://raw.githubusercontent.com/confessess/MM097059417091750917/main/main.lua"
+local FTAPURL = "https://raw.githubusercontent.com/confessess/FTAP097509175091765091750/main/main.lua"
 
 local function safeLoad(url, name)
     local ok, result = pcall(function()
@@ -21,5 +23,15 @@ end
 
 if game.PlaceId == 114234929420007 then
     safeLoad(BloxStrikeURL, "Blox Strike")
+    return
+end
+
+if game.PlaceId == 142823291 then
+    safeLoad(MM2URL, "MM2")
+    return
+end
+
+if game.PlaceId == 6961824067 then
+    safeLoad(FTAPURL, "FTAP")
     return
 end
